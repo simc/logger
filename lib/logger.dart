66e857220d2c6130cd9787922a05860d36413390
Marc Grau Benaiges <marcgraub@gmail.com>
2019-11-04 16:37:34 +0100

    Fix format

--- lib/logger.dart
+++ lib/logger.dart
@@ -11,7 +11,7 @@ export 'src/printers/pretty_printer.dart';
 export 'src/printers/simple_printer.dart';
 
 export 'src/log_output.dart'
-  if (dart.library.io) 'src/outputs/file_output.dart';
+    if (dart.library.io) 'src/outputs/file_output.dart';
 
 export 'src/log_filter.dart';
 export 'src/log_output.dart';
