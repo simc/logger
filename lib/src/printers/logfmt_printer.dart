d045820f216ce25bf73358765599fd2349ac5ac5
Harm Aarts <harm@aarts.email>
2019-12-10 10:25:49 +0100

    Add LogfmtPrinter

---
+++ lib/src/printers/logfmt_printer.dart
@@ -0,0 +1,35 @@
+import 'package:logger/src/log_printer.dart';
+import 'package:logger/src/logger.dart';
+
+/// Outputs a logfmt message:
+/// ```
+/// level=debug msg="hi there" time="2015-03-26T01:27:38-04:00" animal=walrus number=8 tag=usum
+/// ```
+class LogfmtPrinter extends LogPrinter {
+  static final levelPrefixes = {
+    Level.verbose: 'verbose',
+    Level.debug: 'debug',
+    Level.info: 'info',
+    Level.warning: 'warning',
+    Level.error: 'error',
+    Level.wtf: 'wtf',
+  };
+
+  @override
+  List<String> log(LogEvent event) {
+    var output = StringBuffer("level=${levelPrefixes[event.level]}");
+    if (event.message is String) {
+      output.write(' msg="${event.message}"');
+    } else if (event.message is Map) {
+      event.message.entries.forEach((entry) {
+        if (entry.value is num) {
+          output.write(' ${entry.key}=${entry.value}');
+        } else {
+          output.write(' ${entry.key}="${entry.value}"');
+        }
+      });
+    }
+
+    return [output.toString()];
+  }
+}
