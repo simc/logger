a4a193930c8af24ea188f3cf16373bad4e534101
Marc Grau Benaiges <marcgraub@gmail.com>
2020-01-16 16:37:34 +0100

    Make simple_printer.dart export optional
    Hi! Importing dart:io as a dependency makes the package incompatible with browser projects.
    
    One solution can be making simple_printer.dart export optional like file_output.dart
    
    See: https://github.com/leisim/logger/commit/9d6461154d6df042ef09b71f7f9beb97a8aa347a#diff-9efc0241babc652e668a9441d2926a03R2

--- lib/logger.dart
+++ lib/logger.dart
@@ -9,11 +9,13 @@ export 'src/outputs/console_output.dart';
 export 'src/outputs/stream_output.dart';
 export 'src/outputs/memory_output.dart';
 export 'src/printers/pretty_printer.dart';
-export 'src/printers/simple_printer.dart';
 export 'src/printers/logfmt_printer.dart';
 
 export 'src/log_output.dart'
     if (dart.library.io) 'src/outputs/file_output.dart';
+
+export 'src/log_printer.dart'
+    if (dart.library.io) 'src/printers/simple_printer.dart';
 
 export 'src/log_filter.dart';
 export 'src/log_output.dart';
