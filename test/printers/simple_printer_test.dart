30c949ee695b443440e6f58d47bbb9a2a70b6cff
Harm Aarts <harm@aarts.email>
2020-03-27 16:31:33 +0100

    Modify test to comply with new API

--- test/printers/simple_printer_test.dart
+++ test/printers/simple_printer_test.dart
@@ -12,9 +12,7 @@ void main() {
     StackTrace.current,
   );
 
-  var plainPrinter = SimplePrinter()
-    ..useColor = false
-    ..printTime = false;
+  var plainPrinter = SimplePrinter(colors: false, printTime: false);
 
   test('represent event on a single line (ignoring stacktrace)', () {
     var outputs = plainPrinter.log(event);
@@ -27,13 +25,13 @@ void main() {
     test('print color', () {
       // `useColor` is detected but here we override it because we want to print
       // the ANSI control characters regardless for the test.
-      var printer = SimplePrinter()..useColor = true;
+      var printer = SimplePrinter(colors: true);
 
       expect(printer.log(event)[0], contains(ansiEscapeLiteral));
     });
 
     test('toggle color', () {
-      var printer = SimplePrinter()..useColor = false;
+      var printer = SimplePrinter(colors: false);
 
       expect(printer.log(event)[0], isNot(contains(ansiEscapeLiteral)));
     });
@@ -45,8 +43,8 @@ void main() {
     expect(printer.log(event)[0], contains('TIME'));
   });
 
-  test('toggle time', () {
-    var printer = SimplePrinter(printTime: true)..printTime = false;
+  test('does not print time', () {
+    var printer = SimplePrinter(printTime: false);
 
     expect(printer.log(event)[0], isNot(contains('TIME')));
   });
