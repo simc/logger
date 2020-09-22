63a6b266980c8a873534fdc8a78664e642bcdff9
Harm Aarts <harm@aarts.email>
2020-09-22 17:41:59 +0200

    Add docs to MemoryOutput
    Again thanks to @gmpassos.

--- lib/src/outputs/memory_output.dart
+++ lib/src/outputs/memory_output.dart
@@ -5,8 +5,13 @@ import 'package:logger/src/log_output.dart';
 
 /// Buffers [OutputEvent]s.
 class MemoryOutput extends LogOutput {
+  /// Maximum events in [buffer].
   final int bufferSize;
+
+  /// A secondary [LogOutput] to also received events.
   final LogOutput secondOutput;
+
+  /// The buffer of events.
   final ListQueue<OutputEvent> buffer;
 
   MemoryOutput({this.bufferSize = 20, this.secondOutput})
