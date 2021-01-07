546fcaa55e3b3d5b90921c38657ccc5412e4dc83
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-01-07 21:06:10 +0100

    use correct name for conditional import

--- lib/logger.dart
+++ lib/logger.dart
@@ -17,7 +17,8 @@ export 'src/printers/simple_printer.dart';
 export 'src/printers/hybrid_printer.dart';
 export 'src/printers/prefix_printer.dart';
 
-export 'src/log_output.dart' if (dart.io) 'src/outputs/file_output.dart';
+export 'src/log_output.dart'
+    if (dart.library.io) 'src/outputs/file_output.dart';
 
 export 'src/log_filter.dart';
 export 'src/log_output.dart';
