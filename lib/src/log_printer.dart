77cdd828a83491bbfb39836d310f3bd3b507f02f
Bungeefan <severin.hamader@yahoo.de>
2023-02-08 17:16:33 +0100

    Fixed doc reference

--- lib/src/log_printer.dart
+++ lib/src/log_printer.dart
@@ -3,7 +3,7 @@ import 'package:logger/src/logger.dart';
 /// An abstract handler of log events.
 ///
 /// A log printer creates and formats the output, which is then sent to
-/// [LogOutput]. Every implementation has to use the [LogPrinter.println]
+/// [LogOutput]. Every implementation has to use the [LogPrinter.log]
 /// method to send the output.
 ///
 /// You can implement a `LogPrinter` from scratch or extend [PrettyPrinter].
